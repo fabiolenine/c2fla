cfbb25eeb28d16639105231b9bb02f51988aa45b
Fabio Lenine <fabiolenine@gmail.com>
2017-11-09 10:08:56 -0300

    Alter
    The URL validation function was created and deployed on the new short
    url endpoint.

--- routes/routec2fla.js
+++ routes/routec2fla.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
-module.exports = function(app,dbcrud,parameter,base58)
+module.exports = function(app,dbcrud,parameter,base58,utils)
 {
 
   app.get('/', function(req, res){
@@ -10,21 +10,24 @@ module.exports = function(app,dbcrud,parameter,base58)
     var longUrl = req.body.url;
     var shortUrl = '';
 
-    // check if url already exists in database
-    dbcrud.urllongfind(longUrl, function (err, done){
+    if(utils.isvalidurl(longUrl)){
+      // check if url already exists in database
+      dbcrud.urllongfind(longUrl, function (err, done){
 
-      if (done){
-        shortUrl = parameter.webhost + base58.encode(done._id);
-        // the document exists, so we return it without creating a new entry
-      } else {
-        // since it doesn't exist, let's go ahead and create it:
-        dbcrud.urlshortnew(longUrl, function (err, done){
+        if (done){
           shortUrl = parameter.webhost + base58.encode(done._id);
-        });
-      }
-      res.send({'shortUrl': shortUrl});
+          // the document exists, so we return it without creating a new entry
+        } else {
+          // since it doesn't exist, let's go ahead and create it:
+          dbcrud.urlshortnew(longUrl, function (err, done){
+            shortUrl = parameter.webhost + base58.encode(done._id);
+          });
+        }
+        res.send({'shortUrl': shortUrl});
+      });
+    }
+    else res.send({'shortUrl': 'Invalid URL format. Input URL must comply to the following: http(s)://(www.)domain.ext(/)(path)'});
 
-    });
   });
 
   app.get('/:encoded_id', function(req, res){
